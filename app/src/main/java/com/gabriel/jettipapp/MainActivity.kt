7ee7faf90a06974df68ff08cbc07d1f43d0dceb3
gabri <gabriel.amoreira@outlook.com>
2024-06-19 10:56:44 -0300

    Initial commit

---
+++ app/src/main/java/com/gabriel/jettipapp/MainActivity.kt
@@ -0,0 +1,186 @@
+package com.gabriel.jettipapp
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Remove
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.gabriel.jettipapp.components.InputField
+import com.gabriel.jettipapp.ui.theme.JettipAppTheme
+import com.gabriel.jettipapp.widgets.RoundIconButton
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyApp {
+//                TopHeader()
+                MainContent()
+            }
+        }
+    }
+}
+
+@Composable
+fun MyApp(content: @Composable () -> Unit) {
+    JettipAppTheme {
+        Surface(color = MaterialTheme.colorScheme.background) {
+            content()
+        }
+
+    }
+}
+
+@Composable
+fun TopHeader(
+    modifier: Modifier = Modifier,
+    totalPerPerson: Double = 0.00
+) {
+    Surface(
+        modifier
+            .fillMaxWidth()
+            .height(150.dp)
+            .clip(shape = RoundedCornerShape(corner = CornerSize(12.dp))),
+        color = Color(0xFFE9D7F7)
+    ) {
+        Column(
+            modifier.padding(12.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            val total = "%.2f".format(totalPerPerson)
+            Text(
+                text = "Total Por Pessoa",
+                style = MaterialTheme.typography.titleLarge
+            )
+            Text(
+                text = "R$$total",
+                style = MaterialTheme.typography.displayMedium,
+                fontWeight = FontWeight.Bold
+            )
+        }
+    }
+}
+
+@Composable
+fun MainContent(modifier: Modifier = Modifier) {
+    BillForm(){billAmt ->
+        Log.d("AMT", "MainContent: ${billAmt.toInt() * 100}")
+    }
+}
+
+@Composable
+fun BillForm(
+    modifier: Modifier = Modifier,
+    onValueChange: (String) -> Unit = {}
+) {
+    val totalBillState = remember {
+        mutableStateOf("")
+    }
+    val validState = remember(totalBillState.value) {
+        totalBillState.value.trim().isNotEmpty()
+    }
+    val keyboardController = LocalSoftwareKeyboardController.current
+
+    Surface(
+        modifier
+            .padding(2.dp)
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(corner = CornerSize(8.dp)),
+        border = BorderStroke(width = 1.dp, color = Color.LightGray)
+    ) {
+        Column(modifier.padding(6.dp),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.Start) {
+            InputField(
+                valueState = totalBillState,
+                labelId = "Enter Bill",
+                enable = true,
+                isSingleLine = true,
+                onAction = KeyboardActions {
+                    if (!validState) return@KeyboardActions
+                    onValueChange(totalBillState.value.trim())
+                    keyboardController?.hide()
+                }
+            )
+            if(validState){
+                Row(modifier = modifier.padding(3.dp),
+                    horizontalArrangement = Arrangement.Start,
+                    ){
+                    Text(text = "Split",
+                        modifier.align(
+                            alignment = Alignment.CenterVertically
+                        ))
+                    Spacer(modifier = Modifier.width(120.dp))
+                    Row (modifier = Modifier.padding(horizontal = 3.dp),
+                        horizontalArrangement = Arrangement.End){
+                        RoundIconButton(
+                            imageVector = Icons.Default.Remove,
+                            onClick = { /*TODO*/ })
+                        Text(text = "2",
+                            modifier.align(Alignment.CenterVertically)
+                                .padding(start = 9.dp, end = 9.dp))
+                        RoundIconButton(
+                            imageVector = Icons.Default.Add,
+                            onClick = { /*TODO*/ })
+                    }
+                }
+
+            }else{
+                Box(){}
+            }
+        }
+    }
+
+}
+
+
+@Preview
+@Composable
+private fun TopHeaderPreview() {
+    TopHeader()
+}
+
+@Preview
+@Composable
+private fun MainContentPreview() {
+    MainContent()
+}
+
+@Preview
+@Composable
+private fun BillFormPreview() {
+    BillForm()
+}
+
